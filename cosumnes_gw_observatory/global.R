c433e42e9e2a157a01505e10fa40cb2abe1a4c09
richpauloo <richpauloo@gmail.com>
2018-03-07 17:00:01 -0800

    password removed

--- cosumnes_gw_observatory/global.R
+++ cosumnes_gw_observatory/global.R
@@ -18,7 +18,7 @@ cs_coords <- data_frame(lat = c(38.30139, 38.31263, 38.30505, 38.30488, 38.29666
 # connect to mySQL db
 con <- dbConnect(RMySQL::MySQL(),
                  user = "gw_observatory",
-                 password = "ceu9chaiCai3ieb8Kiesh5",
+                 password = "",
                  host = "169.237.35.237",
                  dbname = "gw_observatory",
                  port = 33306)
